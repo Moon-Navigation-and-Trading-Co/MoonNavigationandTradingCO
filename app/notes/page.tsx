1c67330a61a84a4d0143ce674da854213099dfd9
ziad <ziadk432@hotmail.com>
2024-10-04 20:11:50 +0300

    managing session

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,12 +1,22 @@
 import { createClient } from '@/utils/supabase/server'
+import { redirect } from "next/navigation";
 
 export default async function Page() {
     const supabase = createClient()
+
+    const {
+        data: { user },
+    } = await supabase.auth.getUser();
+
+    if (!user) {
+        return redirect("/sign-in");
+    }
+
     const { data: notes } = await supabase.from('notes').select()
 
     const { data, error } = await supabase
         .from('notes')
         .insert([{ title: 'New Note 1' }])
 
-    return <pre>This should work now: {JSON.stringify(notes, null, 2)}</pre>
+    return <pre>This only works if user is logged in: {JSON.stringify(notes, null, 2)}</pre>
 }
