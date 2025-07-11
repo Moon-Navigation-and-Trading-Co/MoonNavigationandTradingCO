22b943f63c0a6b40d2bfef9d674cd2f85c9e5e63
faridaismaill12 <farida.ashraf@hotmail.co.uk>
2025-07-12 00:40:49 +0300

    partners page standalone

---
+++ src/app/[locale]/partners/page.tsx
@@ -0,0 +1,9 @@
+import PartnerLogoCarousel from "@/components/partners-carousel";
+
+export default function PartnersPage(){
+    return(
+        <div className="mt-[30vh]">
+        <PartnerLogoCarousel/>
+        </div>
+    )
+}
